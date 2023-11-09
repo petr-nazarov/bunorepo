147b585a7b7e440b4988eb17437f5b80448d37c7
Petr Nazarov <mrzueck@gmail.com>
2023-11-09 11:12:37 +0000

    ci(github): creating actions

--- packages/config/changesets/commit.cjs
+++ packages/config/changesets/commit.cjs
@@ -1,21 +1,27 @@
-const commit = require('@changesets/cli/commit')
-
+const commit = require("@changesets/cli/commit");
 
 const getVersionMessage = async (releasePlan, options) => {
-  const skipCI = (options === null || options === void 0 ? void 0 : options.skipCI) === "version" || (options === null || options === void 0 ? void 0 : options.skipCI) === true;
-  const publishableReleases = releasePlan.releases.filter(release => release.type !== "none");
-  const numPackagesReleased = publishableReleases.length;
-  const releasesLines = publishableReleases.map(release => `  ${release.name}@${release.newVersion}`).join("\n");
-  return `
+	const skipCI =
+		(options === null || options === void 0 ? void 0 : options.skipCI) ===
+			"version" ||
+		(options === null || options === void 0 ? void 0 : options.skipCI) === true;
+	const publishableReleases = releasePlan.releases.filter(
+		(release) => release.type !== "none",
+	);
+	const numPackagesReleased = publishableReleases.length;
+	const releasesLines = publishableReleases
+		.map((release) => `  ${release.name}@${release.newVersion}`)
+		.join("\n");
+	return `
     chore(release): releasing ${numPackagesReleased} package(s)
 
     Releases:
     ${releasesLines}
-    ${skipCI ? `\n[skip ci]\n` : ""}
+    ${skipCI ? "\n[skip ci]\n" : ""}
 `;
 };
 
 module.exports = {
-  getAddMessage: commit.default.getAddMessage,
-  getVersionMessage: getVersionMessage
-}
+	getAddMessage: commit.default.getAddMessage,
+	getVersionMessage: getVersionMessage,
+};
